1396874e7d8db90c35cfd5dcb886da0ec7304c72
dmarsden <daniel.marsden@sdl.usu.edu>
2019-11-12 09:29:35 -0700

    add complete dataset

---
+++ public/readData.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function readInDataItem(itemName, fileLoadedCallback){
+  // TODO iterate through each directory and load all
+  // files inside
+  // d3.csv("data/<filename.csv>").then(fileLoadedCallback);
+}
