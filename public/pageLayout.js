1d19d55117535e0824867c3d8cf1cd5c48cd24ac
Daniel Marsden <marsden.daniel@gmail.com>
2019-11-13 10:09:28 -0700

    load checklist dynamically, update onItemChecked to access the  file data

--- public/pageLayout.js
+++ public/pageLayout.js
@@ -1,5 +1,61 @@
 "using strict";
 
+let itemList = [
+  { label: "Alarmclock", dir: "Alarmclock" },
+  { label: "Amplifier", dir: "Amplifier" },
+  { label: "Bean To Cup Coffee Maker", dir: "BeanToCupCoffeemaker" },
+  { label: "Bread Cutter", dir: "Breadcutter" },
+  { label: "Cd Player", dir: "CdPlayer" },
+  { label: "Charger PSP", dir: "Charger-PSP" },
+  { label: "Charger Smartphone", dir: "Charger-Smartphone" },
+  { label: "Coffee Maker", dir: "Coffeemaker" },
+  { label: "Cooking Stove", dir: "Cookingstove" },
+  { label: "Digital Tv Receiver", dir: "DigitalTvReceiver" },
+  { label: "Dishwasher", dir: "Dishwasher" },
+  { label: "Dvd Player", dir: "DvdPlayer" },
+  { label: "Ethernet Switch", dir: "EthernetSwitch" },
+  { label: "Freezer", dir: "Freezer" },
+  { label: "Iron", dir: "Iron" },
+  { label: "Lamp", dir: "Lamp" },
+  { label: "Laundry Dryer", dir: "LaundryDryer" },
+  { label: "Microwave Oven", dir: "MicrowaveOven" },
+  { label: "Monitor CRT", dir: "Monitor-CRT" },
+  { label: "Monitor TFT", dir: "Monitor-TFT" },
+  { label: "Multi-media Center", dir: "Multimediacenter" },
+  { label: "PC Desktop", dir: "PC-Desktop" },
+  { label: "PC Laptop", dir: "PC-Laptop" },
+  { label: "Playstation3", dir: "Playstation3" },
+  { label: "Printer", dir: "Printer" },
+  { label: "Projector", dir: "Projector" },
+  { label: "Refrigerator", dir: "Refrigerator" },
+  { label: "Remote Desktop", dir: "RemoteDesktop" },
+  { label: "Router", dir: "Router" },
+  { label: "Solar Thermal System", dir: "SolarThermalSystem" },
+  { label: "Subwoofer", dir: "Subwoofer" },
+  { label: "Toaster", dir: "Toaster" },
+  { label: "TV CRT", dir: "TV-CRT" },
+  { label: "TV LCD", dir: "TV-LCD" },
+  { label: "USB Harddrive", dir: "USBHarddrive" },
+  { label: "USB Hub", dir: "USBHub" },
+  { label: "Vacuum Cleaner", dir: "VacuumCleaner" },
+  { label: "Video Projector", dir: "VideoProjector" },
+  { label: "Washing Machine", dir: "Washingmachine" },
+  { label: "Water Boiler", dir: "WaterBoiler" },
+  { label: "Water Fountain", dir: "WaterFountain" },
+  { label: "Water Kettle", dir: "WaterKettle" },
+  { label: "Xmas Lights", dir: "XmasLights" }
+];
+
+function pageInit() {
+  var itemSelection = d3.select("#mySidebar").selectAll("label").data(itemList);
+  itemSelection.enter()
+    .append("label")
+    .html(d => {
+      return `<p> <input type="checkbox" value="${d.dir}" onclick="onItemChecked(this)" /> ${d.label} </p>`;
+    })
+    ;
+}
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
--- public/readData.js
+++ public/readData.js
@@ -1,7 +1,52 @@
 "use strict";
 
+// format [{label: data: []}....]
+let g_dataset = [];
+
+function onItemChecked(item){
+  console.log("checked:" + item.value + " - reading in data...");
+  if (item.checked) {
+    readInDataItem(item.value);
+  } else {
+    // TODO remove data from dataset
+  }
+}
+
 function readInDataItem(itemName, fileLoadedCallback){
   // TODO iterate through each directory and load all
   // files inside
-  // d3.csv("data/<filename.csv>").then(fileLoadedCallback);
+  /*
+  "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN"><html>
+    <title>Directory listing for /data/Alarmclock/</title>
+    <body>
+    <h2>Directory listing for /data/Alarmclock/</h2>
+    <hr>
+    <ul>
+    <li><a href="dev_59AC89_2011.09.01.csv">dev_59AC89_2011.09.01.csv</a>
+    <li><a href="dev_59AC89_2011.09.02.csv">dev_59AC89_2011.09.02.csv</a>
+    <li><a href="dev_59AC89_2011.09.03.csv">dev_59AC89_2011.09.03.csv</a>
+    <li><a href="dev_59AC89_2011.09.04.csv">dev_59AC89_2011.09.04.csv</a>
+    <li><a href="dev_59AC89_2011.09.05.csv">dev_59AC89_2011.09.05.csv</a>
+    </ul>
+    <hr>
+    </body>
+    </html>
+    " 
+  */
+  $.ajax({
+    url: `/data/${itemName}/`,
+    success: function(data){
+      var found = $(data).find("li > a");
+      _.each(found, function (d) {
+        var filename = d.innerHTML;
+        d3.csv(`data/${itemName}/${filename}`).then(function (fileData){
+          // Example:
+          // 0: {01/09/2011 00:00:02;0;0: "01/09/2011 00:00:04;2;0"}
+          // console.log("file data ...");
+          // TODO parse each file field and place into the data set
+          // in a easy way to retrieve it
+        });
+      });
+    }
+  }); 
 }
