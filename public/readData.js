577d21df71450a5e9879541297d0b62aedf06879
Daniel Marsden <marsden.daniel@gmail.com>
2019-11-16 07:02:57 -0700

    load some data up front

--- public/readData.js
+++ public/readData.js
@@ -1,18 +1,19 @@
 "use strict";
 
 // format [{label: data: []}....]
-let g_dataset = [];
+//$ python -m SimpleHTTPServer 8080
+let g_dataset = {};
 
 function onItemChecked(item){
   console.log("checked:" + item.value + " - reading in data...");
   if (item.checked) {
-    readInDataItem(item.value);
+    readInDataItem(item.value, item.label);
   } else {
-    // TODO remove data from dataset
+    // TODO remove data from dataset - set that it is not selected
   }
 }
 
-function readInDataItem(itemName, fileLoadedCallback){
+function readInDataItem(itemName, label){
   // TODO iterate through each directory and load all
   // files inside
   /*
@@ -37,16 +38,37 @@ function readInDataItem(itemName, fileLoadedCallback){
     url: `/data/${itemName}/`,
     success: function(data){
       var found = $(data).find("li > a");
-      _.each(found, function (d) {
-        var filename = d.innerHTML;
-        d3.csv(`data/${itemName}/${filename}`).then(function (fileData){
-          // Example:
-          // 0: {01/09/2011 00:00:02;0;0: "01/09/2011 00:00:04;2;0"}
-          // console.log("file data ...");
-          // TODO parse each file field and place into the data set
-          // in a easy way to retrieve it
-        });
-      });
+      // console.log("checked:" + itemName + " - reading in data...");
+      // _.each(found, function (d) {
+        var fileCount = 1;
+        for (var i = 0; i < found.length; i++)
+        {
+          var filename = found[i].innerHTML;
+          d3.csv(`data/${itemName}/${filename}`).then(function (fileData){
+            // Example input:
+            // 0: {01/09/2011 00:00:02;0;0: "01/09/2011 00:00:04;2;0"}
+            // 1: {01/09/2011 00:00:02;0;0: "01/09/2011 00:00:04;2;0"}
+            // console.log("file data ...");
+            // Item Format: {itemName: {label, selected, days: []}}
+            // Data format: {day, time, val, valAverage}
+  
+            var dayData = _.map(fileData, function (row) {
+              var str1 = row[_.keys(row)[0]].split(";");
+              var dateStr = str1[0].split(" ");
+              return {day: dateStr[0], time: dateStr[1], 
+                      val: parseInt(str1[1]), valAverage: parseInt(str1[2])};
+            });
+            
+            if (g_dataset[itemName]) { // if the item is already defined
+              g_dataset[itemName].days.push(dayData);
+            } else {
+              g_dataset[itemName] = {label: label, selected: true, days: []};
+              g_dataset[itemName].days.push(dayData);
+            }
+            console.log(`read data: ${itemName} file ${fileCount} of ${found.length}`);
+            fileCount++;
+          });
+        }
     }
   }); 
 }
