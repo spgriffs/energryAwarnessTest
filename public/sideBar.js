fa17d2df56c106412e229ebdad1943fa2cfa3ce6
Daniel Marsden <marsden.daniel@gmail.com>
2019-11-22 20:36:53 -0700

    hot fix

--- public/sideBar.js
+++ public/sideBar.js
@@ -1,12 +1,12 @@
 
-function open() {
+function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("main").style.visibility = "hidden";
     document.getElementById("content").style.marginLeft = "250px";
 }
 
-function close() {
+function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
     document.getElementById("content").style.marginLeft = "0";
